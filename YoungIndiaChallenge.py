c06412e7dcd03a4ff3b6bb48da14f83ca05145cd
Arpit Nandwani <arpit.nandwani@gmail.com>
2017-04-10 15:05:16 +0530

    Inserted users into table

--- YoungIndiaChallenge.py
+++ YoungIndiaChallenge.py
@@ -28,13 +28,14 @@ Session(app)
 @login_required
 def index():
     if session["auth_lvl"]    == 0:
-        render_template("index_a.html")
+        return render_template("index_a.html")
     elif session["auth_lvl"]  == 1:
-        render_template("index_m.html")
+        return render_template("index_m.html")
     elif session["auth_lvl"]  == 2:
-        render_template("index_i.html")
+        return render_template("index_i.html")
     else:
-        render_template("index.html")
+        return render_template("index.html")
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -46,27 +47,73 @@ def login():
     # if user reached route via POST (as by submitting a form via POST)
     if request.method == 'POST':
         if not request.form.get("username") or not request.form.get("password"):
-            render_template("failure.html", msg="Username/Password fields cannot be empty")
+            return render_template("failure.html", msg="Username/Password fields cannot be empty")
 
         # query database for username
         db = mysql.connection.cursor()
         rows = db.execute("SELECT * FROM users WHERE uname = '{}'".format(request.form.get("username")))
         rv = db.fetchone()
+
+        # verify password
         if rows or rv["pass"] == encode(hashlib.sha1(encode(request.form.get("password", 'utf-8'))).digest(),
                                         'hex_codec').decode('utf-8'):
+            # create session
             session["user_id"]  = rv["id"]
             session["auth_lvl"] = int(rv["authlvl"])
             return redirect(url_for("index"))
+
         else:
             return render_template("failure.html", msg="Invalid Username And/Or Password")
 
+    # else if user reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return None
+    """Register User"""
+
+    # forget any user_id
+    session.clear()
+
+    # if user reached route via POST (as by submitting a form via POST)
+    if request.method == 'POST':
+
+        # check if form fields are empty and if entered passwords match
+        if not request.form.get("username") or not request.form.get("password"):
+            return render_template("failure.html", msg="Username/Password fields cannot be empty")
+        if request.form.get("password") != request.form.get("confirmation"):
+            return render_template("failure.html", msg="Password fields do not match")
+
+        # create connection
+        db = mysql.connection.cursor()
+
+        # query database to see if username already exists
+        rows = db.execute(
+                "SELECT * FROM users WHERE uname = '{0}' UNION SELECT * FROM tempusers WHERE uname = '{1}'".format(
+                    request.form.get("username"), request.form.get("username")))
+        if rows:
+            return render_template("failure.html", msg="Username Already Exists")
+
+        # hash password with SHA-1 algorithm and store it as string
+        password = encode(hashlib.sha1(encode(request.form.get("password", 'utf-8'))).digest(),
+                          'hex_codec').decode('utf-8')
+
+
+        if int(request.form.get("auth")) in {0, 1, 2}:
+            db.execute("INSERT INTO tempusers (uname, pass, authlvl) VALUES ('{0}', '{1}', '{2}')".format(
+                            request.form.get("username"), password, request.form.get("auth")))
+        else:
+            db.execute( "INSERT INTO users (uname, pass, authlvl) VALUES ('{0}', '{1}', '{2}')".format(
+                            request.form.get("username"), password, request.form.get("auth")))
+        mysql.connection.commit()
+
+        return render_template("failure.html", msg="Added")
+
+    # else if user reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
 
 
 if __name__ == '__main__':
