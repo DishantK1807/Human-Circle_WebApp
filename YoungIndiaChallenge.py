6b910f8ad1bef9da93a3b404e67dfc781fe967e4
Arpit Nandwani <arpit.nandwani@gmail.com>
2017-04-10 11:25:16 +0530

    Implemented index route

--- YoungIndiaChallenge.py
+++ YoungIndiaChallenge.py
@@ -10,30 +10,31 @@ from helpers import *
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '456123'
-app.config['MYSQL_DB'] = 'yic'
+app.config['MYSQL_HOST']        = 'localhost'
+app.config['MYSQL_USER']        = 'root'
+app.config['MYSQL_PASSWORD']    = '456123'
+app.config['MYSQL_DB']          = 'yic'
 app.config['MYSQL_CURSORCLASS'] = "DictCursor"
 mysql = MySQL(app)
 
 # configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = gettempdir()
+app.config["SESSION_FILE_DIR"]  = gettempdir()
 app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
+app.config["SESSION_TYPE"]      = "filesystem"
 Session(app)
 
 
-@app.route('/')
+@app.route("/")
 @login_required
 def index():
-    if session["authlvl"] == 1:
+    if session["auth_lvl"]    == 0:
+        render_template("index_a.html")
+    elif session["auth_lvl"]  == 1:
         render_template("index_m.html")
-    elif session["authlvl"] == 2:
+    elif session["auth_lvl"]  == 2:
         render_template("index_i.html")
     else:
-        render_template("index_c.html")
-
+        render_template("index.html")
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -53,12 +54,11 @@ def login():
         rv = db.fetchone()
         if rows or rv["pass"] == encode(hashlib.sha1(encode(request.form.get("password", 'utf-8'))).digest(),
                                         'hex_codec').decode('utf-8'):
-
-            redirect(url_for(index))
+            session["user_id"]  = rv["id"]
+            session["auth_lvl"] = int(rv["authlvl"])
+            return redirect(url_for("index"))
         else:
             return render_template("failure.html", msg="Invalid Username And/Or Password")
-
-            # return redirect(url_for(index))
 
     else:
         return render_template("login.html")
