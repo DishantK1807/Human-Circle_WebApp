db3fc870a54bc19807cbe639be95e64be4e8def5
Arpit Nandwani <arpit.nandwani@gmail.com>
2017-08-31 02:56:58 +0530

    Added Comments

--- App/views/home.py
+++ App/views/home.py
@@ -19,6 +19,7 @@ from App.views import interviewer
 @app.route('/', methods=['GET', 'POST'])
 @login_required
 def index():
+    # Admin
     if session.get('auth_lvl')      == 0:
         if request.method == 'POST':
             return admin.add(request.form.get('email'), request.form.get("approval"))
@@ -27,11 +28,11 @@ def index():
             db.execute("SELECT * FROM tempusers ORDER BY fname")
             return render_template('admin/index.html', rows=db.fetchall())
 
-
+    # Mentor
     elif session.get('auth_lvl')    == 1:
         return render_template('mentor/index.html')
 
-
+    # Interviewer
     elif session.get('auth_lvl')    == 2:
         if request.method == 'POST':
             if app.config['stage'] == 2:
@@ -57,7 +58,7 @@ def index():
             else:
                 return render_template('failure.html', msg="This phase has ended")
 
-
+    # Candidate
     else:
         db = mysql.connection.cursor()
         db.execute("SELECT * FROM candidates WHERE uid = {}".format(session['user_id']))
