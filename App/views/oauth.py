c415d0e5de0753ab0745b0fed00fafd48c6fafbd
Arpit Nandwani <arpit.nandwani@gmail.com>
2017-11-26 00:05:43 +0530

    Updated Google Auth Methods

--- App/views/oauth.py
+++ App/views/oauth.py
@@ -14,14 +14,14 @@ def add_user(id, email, password, fname, lname):
 
 GOOGLE_CLIENT_ID = '418555457379-s4qrcjttitvokgckatucvsmd2dvokt44.apps.googleusercontent.com'
 GOOGLE_CLIENT_SECRET = 'onAkS2oPYrHTBi4_l6EhLZn4'
-REDIRECT_URI = '/oauth2callback'
+REDIRECT_URI = '/googlecallback'
 
 
 google = oauth.remote_app('google',
                           base_url='https://www.googleapis.com/oauth2/v1/',
                           authorize_url='https://accounts.google.com/o/oauth2/auth',
                           request_token_url=None,
-                          request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email'},
+                          request_token_params={'scope': 'email'},
                           access_token_url='https://accounts.google.com/o/oauth2/token',
                           access_token_method='POST',
                           consumer_key=GOOGLE_CLIENT_ID,
@@ -29,13 +29,15 @@ google = oauth.remote_app('google',
 
 @app.route('/googleauth')
 def googleauth():
-    callback=url_for('authorized', _external=True)
+    callback=url_for('googlecallback', _external=True)
     return google.authorize(callback=callback)
 
 
 @app.route(REDIRECT_URI)
 @google.authorized_handler
-def authorized(resp):
+def googlecallback(resp):
+    if resp is None:
+        return render_template('failure.html', msg='No response from Google account')
     access_token = resp['access_token']
     session['token'] = access_token, ''
     user_data = google.get('userinfo').data
